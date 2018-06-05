cdadc3abf10aa43e2acab7ec4bfac487d865080e
thuantran73 <larten1234@gmail.com>
2018-06-05 13:20:10 +0700

    Add Meassage Functions

---
+++ app/src/main/java/com/larten/diff/MessageAdapter.java
@@ -0,0 +1,92 @@
+package com.larten.diff;
+
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.larten.diff.Messages;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageViewHolder> {
+    private List<Messages> mMessageList;
+    private DatabaseReference mUserDatabase;
+    private FirebaseAuth mAuth;
+
+
+    public MessageAdapter(List<Messages> mMessageList){
+        this.mMessageList = mMessageList;
+    }
+
+    @NonNull
+    @Override
+    public MessageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.message_single_layout, parent, false);
+        return new MessageViewHolder(v);
+    }
+    public class MessageViewHolder extends RecyclerView.ViewHolder{
+        public TextView messageText;
+        public CircleImageView profileImage;
+        public MessageViewHolder (View view){
+            super(view);
+
+            messageText = (TextView) view.findViewById(R.id.message_text_layout);
+            profileImage = (CircleImageView) view.findViewById(R.id.messages_profile_layout);
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final MessageViewHolder holder, int position) {
+        mAuth = FirebaseAuth.getInstance();
+        String current_user_id = mAuth.getCurrentUser().getUid();
+
+        Messages c = mMessageList.get(position);
+
+        String from_user = c.getFrom();
+
+        if (from_user.equals(current_user_id)){
+            holder.messageText.setBackgroundColor(Color.GRAY);
+            holder.messageText.setTextColor(Color.BLACK);
+
+        }
+        else {
+            holder.messageText.setBackgroundResource(R.drawable.message_text_background);
+            holder.messageText.setTextColor(Color.BLACK);
+        }
+
+        holder.messageText.setText(c.getMessage());
+        mUserDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(from_user);
+        mUserDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String image = dataSnapshot.child("thumb_image").getValue().toString();
+                Picasso.with(holder.profileImage.getContext()).load(image).placeholder(R.drawable.df_ninja).into(holder.profileImage);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+
+        return mMessageList.size();
+    }
+
+}
